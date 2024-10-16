c2b7dc774a7673abf526de030dbff752bd0121ee
Elena Serova <serova74@bk.ru>
2024-10-16 11:38:05 +0300

    corrections11.1
    Signed-off-by: Elena Serova <serova74@bk.ru>

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -133,3 +133,66 @@ def test_transaction_descriptions():
     trans_descr = transaction_descriptions(transactions)
     assert next(trans_descr) == "Перевод организации"
     assert next(trans_descr) == "Перевод со счета на счет"
+
+
+@pytest.mark.parametrize("start, stop, expected", [(1, 1, ("0000 0000 0000 0001"))])
+def test_card_number_generator(start, stop, expected):
+    assert next(card_number_generator(start, stop)) == expected
+
+
+@pytest.mark.parametrize('index, expected', [(0, 'Перевод организации'), (1, 'Перевод со счета на счет')])
+
+def test_transaction_descriptions_3(index, expected):
+
+    transactions = [
+
+        {'description': 'Перевод организации'},
+
+        {'description': 'Перевод со счета на счет'}
+
+    ]
+
+    descriptions = list(transaction_descriptions(transactions))
+
+    assert descriptions[index] == expected
+
+
+@pytest.fixture
+def transactions():
+    return [{
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {
+            "amount": "9824.07",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702"
+    },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        }
+    ]
+
+
+def test_filter_by_currency(transactions):
+    result = filter_by_currency(transactions, "RUB")
+    assert list(result) == []
+
