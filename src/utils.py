d87b678c834e9b6c4657c94bfc9353955b954b7a
Elena Serova <serova74@bk.ru>
2024-11-04 21:38:15 +0300

    corrections12.2
    Signed-off-by: Elena Serova <serova74@bk.ru>

--- src/utils.py
+++ src/utils.py
@@ -1,24 +1,43 @@
 import json
 import logging
-import sys
-from pathlib import Path
-from typing import List
+import os
+from typing import Any
 
-sys.path.append(str(Path(__file__).resolve().parent.parent))
+# Получаем абсолютный путь до текущей директории
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
-def get_transactions(path_to_file: Path) -> List:
-    """Функция, возвращающая из json-файла данные о транзакциях"""
-    try:
-        with open(path_to_file) as json_file:
-            try:
-                # logger.info(f"Открываем json-файл {path_to_file}")
-                transactions = json.load(json_file)
-                return transactions
-            except json.JSONDecodeError:
-                # logger.error("Ошибка декодирования JSON")
-                print("Ошибка декодирования JSON")
-                return []
-    except FileNotFoundError:
-        # logger.error("Файл не найден")
-        print("Файл не найден")
-        return []
+# Создаем путь до файла логов относительно текущей директории
+rel_log_file_path = os.path.join(current_dir, "../logs/utils.log")
+abs_log_file_path = os.path.abspath(rel_log_file_path)
+
+# Создаем путь до файла JSON относительно текущей директории
+rel_src_file_path = os.path.join(current_dir, "../data/operations.json")
+abs_src_file_path = os.path.abspath(rel_src_file_path)
+
+# Добавляем логгер, который записывает логи в файл.
+logger = logging.getLogger("utils")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(abs_log_file_path, "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
+def get_transactions_info_json(json_file: str) -> list[Any]:
+    """Функция принимает на вход путь до JSON-файла и возвращает данные"""
+
+    with open(json_file, "r", encoding="utf-8") as file:
+        try:
+            logger.info("Путь до файла json верный")
+            transactions_info = json.load(file)
+            return transactions_info
+        except:
+            logger.warning("Импортируемый список пуст или отсутствует.")
+            return []
+
+
+print(
+    json.dumps(
+        get_transactions_info_json(abs_src_file_path),
+        indent=4,
+    ))
