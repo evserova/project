e229b9087a773fcf1aadfd04bb4bac1dffee50d2
Elena Serova <serova74@bk.ru>
2024-11-21 00:31:08 +0300

    corrections13.2
    Signed-off-by: Elena Serova <serova74@bk.ru>

---
+++ src/trans.py
@@ -0,0 +1,43 @@
+import re
+from collections import Counter
+
+
+def trans_search(transactions, string_search):
+    '''
+    принимает список словарей с данными о банковских операциях и строку поиска, а возвращает список словарей,
+    у которых в описании есть данная строка
+    :param transactions:
+    :param string_search:
+    :return:
+    '''
+    list_result = []
+    pattern = re.compile(re.escape(string_search), re.I)
+
+    for transaction in transactions:
+        description = transaction.get('description')
+        if isinstance(description, str) and re.search(pattern, description):
+            list_result.append(transaction)
+
+    return list_result
+
+
+def count_trans(list_trans, list_category):
+    '''
+    принимает список словарей с данными о банковских операциях и список категорий операций, а возвращает словарь,
+    в котором ключи — это названия категорий, а значения — это количество операций в каждой категории.
+    :param list_trans:
+    :param list_category:
+    :return:
+    '''
+
+    count_operation = Counter()
+
+    for transaction in list_trans:
+        description = transaction['description']
+
+        for category in list_category:
+            if category.lower() in description.lower():
+                count_operation[category] += 1
+                break
+
+    return count_operation
